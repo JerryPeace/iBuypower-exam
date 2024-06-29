0fbb6f3912051340193beb68f7ed0babf16d34be
jerry.wang2@hp.com <jerry.wang2@hp.com>
2024-06-29 23:46:24 +0800

    chore: modify image auto fit content

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -5,20 +5,22 @@ import Slider from 'react-slick';
 import { Computer } from 'types/computer';
 
 const SliderComp = ({ data }: { data: Computer[] }) => {
-  const [isDesktop, setIsDesktop] = useState(true);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [viewSlidePerShow, setSlidePerShow] = useState(4);
   let sliderRef = useRef<Slider | null>(null);
   const next = () => sliderRef.current?.slickNext();
   const previous = () => sliderRef.current?.slickPrev();
   const settings = {
-    centerMode: true,
     centerPadding: '0',
-    slidesToShow: isDesktop ? 4 : 1,
+    slidesToShow: viewSlidePerShow,
+    initialSlide: 0,
     swipeToSlide: true,
+    infinite: false,
   };
 
   useEffect(() => {
     const updateView = () => {
-      setIsDesktop(window.innerWidth >= 1024);
+      setSlidePerShow(window.innerWidth <= 640 ? 1 : 4);
     };
     updateView();
     window.addEventListener('resize', updateView);
@@ -28,11 +30,19 @@ const SliderComp = ({ data }: { data: Computer[] }) => {
   return (
     <div>
       <div className="text-right">
-        <button className="btn btn-sm btn-outline mx-1 w-9" onClick={previous}>
-          <FaAngleLeft className="text-xl" />
+        <button
+          disabled={currentSlide === 0}
+          className="btn btn-outline mx-1 shadow-md border-gray-300"
+          onClick={previous}
+        >
+          <FaAngleLeft className="text-2xl" />
         </button>
-        <button className="btn btn-sm btn-outline mx-1 w-9" onClick={next}>
-          <FaAngleRight className="text-xl" />
+        <button
+          disabled={currentSlide === (data.length - viewSlidePerShow)}
+          className="btn btn-outline mx-1 shadow-md border-gray-300"
+          onClick={next}
+        >
+          <FaAngleRight className="text-2xl" />
         </button>
       </div>
       <div className="slider-container">
@@ -40,6 +50,7 @@ const SliderComp = ({ data }: { data: Computer[] }) => {
           ref={(slider) => {
             sliderRef.current = slider;
           }}
+          beforeChange={(_, nextSlide: number) => setCurrentSlide(nextSlide)}
           {...settings}
         >
           {data.map((computer: Computer, index) => (
@@ -51,13 +62,14 @@ const SliderComp = ({ data }: { data: Computer[] }) => {
                       {computer.type === 'prebuilt' ? 'Prebuilt PC' : 'Custom PC'}
                     </span>
                   </div>
-                  <div className="w-full h-60 my-5 relative">
+                  <div className="w-auto h-60 my-5 relative">
                     <Image
                       src={computer.image}
                       alt={computer.name}
-                      layout="fill"
-                      objectFit="cover"
-                      className="rounded"
+                      fill
+                      priority
+                      sizes='(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw'
+                      style={{ objectFit: 'contain' }}
                     />
                   </div>
                   <h2 className="text-md font-bold mb-6 truncate">{computer.name}</h2>
@@ -79,18 +91,19 @@ const SliderComp = ({ data }: { data: Computer[] }) => {
                     <span className="text-2xl font-bold mr-2">${computer.price}</span>
                     <span className="line-through text-gray-400">${computer.oldPrice}</span>
                   </div>
-                  <p className="flex text-sm text-blue-500 mb-6">
+                  <div className="flex text-sm text-blue-500 mb-6">
                     <span>{`Starting at ${computer.startingAt}/mo with`}</span>
                     <div className="h-5 w-12 mx-1 relative">
                       <Image
                         src={'/svgs/icon-affirm.svg'}
-                        alt={'company logo'}
-                        layout="fill"
-                        objectFit="cover"
+                        alt={'affirm logo'}
+                        fill
+                        priority
+                        style={{ objectFit: 'cover' }}
                         className="rounded"
                       />
                     </div>
-                  </p>
+                  </div>
                   <div className="flex justify-between items-center mb-2">
                     <div>
                       <p className="text-sm text-gray-600 font-bold">Free Shipping</p>
