cc2c1e01a3722782e6803d659fc82a9a1144392b
jerry.wang2@hp.com <jerry.wang2@hp.com>
2024-06-30 07:13:01 +0800

    test: fix test warning

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,13 @@
 import '@testing-library/jest-dom';
 import 'jest-extended';
-import 'jest-localstorage-mock';
+
+global.matchMedia = global.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  };
+};
--- src/__tests__/computerList.test.tsx
+++ src/__tests__/computerList.test.tsx
@@ -1,57 +1,71 @@
-import { render, screen, waitFor, cleanup, act } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import Home from '@@pages/index';
+import { Computer } from '@@types/computer';
+import { render, screen } from '@testing-library/react';
+import Slider from '../components/Slider';
 
-const mockData = {
-  statusCode: 200,
-  message: 'Successfully retrieved the contacts list',
-  data: [
-    {
-      id: 1,
-      type: 'prebuilt',
-      name: 'Creator RDY Y40BG201',
-      os: 'Windows 11 Pro',
-      cpu: 'Intel® Core™ i9-13900KF CPU',
-      gpu: 'GeForce RTX 4090 - 24GB',
-      storage: '2TB M.2 NVMe GEN4 SSD',
-      ram: '32GB DDR5-6000MHz RGB RAM',
-      price: 3549,
-      oldPrice: 3999,
-      button: 'Buy Now',
-      startingAt: 172.07,
-      image: '/images/gaming-pc-01-Y70-VCTA-B002-MainSolo-400.avif',
-    },
-  ],
-};
+const computers: Computer[] = [
+  {
+    id: 1,
+    type: 'prebuilt',
+    name: 'Creator RDY Y40BG201',
+    os: 'Windows 11 Pro',
+    cpu: 'Intel® Core™ i9-13900KF CPU',
+    gpu: 'GeForce RTX 4090 - 24GB',
+    storage: '2TB M.2 NVMe GEN4 SSD',
+    ram: '32GB DDR5-6000MHz RGB RAM',
+    price: 3549,
+    oldPrice: 3999,
+    button: 'Buy Now',
+    startingAt: 172.07,
+    image: '/images/gaming-pc-01-Y70-VCTA-B002-MainSolo-400.avif',
+  },
+  {
+    id: 2,
+    type: 'custom',
+    name: 'Gaming RDY SLMRG211',
+    os: 'Windows 11 Home',
+    cpu: 'AMD Ryzen 7 7700X CPU',
+    gpu: 'GeForce RTX 4070 - 12GB',
+    storage: '1TB M.2 NVMe SSD',
+    ram: '32GB DDR5-5600MHz RGB RAM',
+    price: 1799,
+    oldPrice: 1999,
+    button: 'Buy Now',
+    startingAt: 87.22,
+    image: '/images/gaming-pc-01-Y70-VCTA-B002-MainSolo-400.avif',
+  },
+];
 
-describe('Home Page with mocked getAllContacts', () => {
-  beforeAll(() => {
-    jest.mock('@@src/apis/clients/computersAPI', () => ({
-      ...jest.requireActual('@@src/apis/clients/computersAPI'),
-      getAllContacts: jest.fn().mockResolvedValue(mockData),
-    }));
-  });
-
-  beforeEach(() => {
-    const queryClient = new QueryClient();
-    act(() => {
-      render(
-        <QueryClientProvider client={queryClient}>
-          <Home />
-        </QueryClientProvider>
-      );
+describe('Slider', () => {
+  it('displays the correct computer name on each slide', () => {
+    render(<Slider data={computers} />);
+    computers.forEach((computer) => {
+      const slideElement = screen.getByText(computer.name);
+      expect(slideElement).toBeInTheDocument();
     });
   });
 
-  afterEach(cleanup);
-
-  test('renders product names from API', async () => {
-    await waitFor(() => expect(screen.getByText('Prebuilt PC')).toBeInTheDocument(), {
-      timeout: 5000,
+  it('displays the correct computer price on each slide', () => {
+    render(<Slider data={computers} />);
+    computers.forEach((computer) => {
+      const slideElement = screen.getByText(`$${computer.price}`);
+      expect(slideElement).toBeInTheDocument();
     });
-    mockData.data.forEach((product) => {
-      const fullName = `${product.name}`;
-      expect(screen.getByText(fullName)).toBeInTheDocument();
+  });
+
+  it('displays the correct computer type on each slide', () => {
+    render(<Slider data={computers} />);
+    computers.forEach((computer) => {
+      const type = computer.type === 'prebuilt' ? 'Prebuilt PC' : 'Custom PC'
+      const slideElement = screen.getByText(type);
+      expect(slideElement).toBeInTheDocument();
+    });
+  });
+
+  it('displays the correct computer image on each slide', () => {
+    render(<Slider data={computers} />);
+    computers.forEach((computer) => {
+      const slideElement = screen.getByAltText(computer.name);
+      expect(slideElement).toBeInTheDocument();
     });
   });
 });
