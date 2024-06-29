0ea1ddaf9c09f6036d3f5f9188b26416a07b9907
jerry.wang2@hp.com <jerry.wang2@hp.com>
2024-06-29 08:55:54 +0800

    chore: add data types

---
+++ types/computer.ts
@@ -0,0 +1,15 @@
+export interface Computer {
+  id: number;
+  type: string;
+  name: string;
+  os: string;
+  cpu: string;
+  gpu: string;
+  storage: string;
+  ram: string;
+  price: number;
+  oldPrice: number;
+  button: string;
+  startingAt: number;
+  image: string;
+}
