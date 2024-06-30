45c3c8d8ca0873c76341a9af17b46ad362aa0f4a
jerry.wang2@hp.com <jerry.wang2@hp.com>
2024-06-30 14:59:03 +0800

    fix: change base path

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const NODE_ENV = process.env.NODE_ENV;
-export const NEXT_PUBLIC_BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '/iBuypower';
-export const NEXT_CLIENT_API_PATH = process.env.NEXT_CLIENT_API_PATH || `/api/v1`;
+export const NEXT_PUBLIC_BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
+export const NEXT_CLIENT_API_PATH = process.env.NEXT_CLIENT_API_PATH || `${NEXT_PUBLIC_BASE_PATH}/api/v1`;
 
 export const config = {
   NEXT_CLIENT_API_PATH,
